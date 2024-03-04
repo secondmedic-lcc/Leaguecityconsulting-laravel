b47025250b9f3c4a32bfe6c2979e28bee3934e3c
rjshah1902 <rjshah1902@gmail.com>
2024-03-04 17:16:23 +0530

    done

--- league-city/resources/views/frontend/layouts/header.blade.php
+++ league-city/resources/views/frontend/layouts/header.blade.php
@@ -22,9 +22,9 @@
                         {{-- <li class="nav-item">
                             <a class="nav-link" href="{{ url('products'); }}">Products</a>
                         </li> --}}
-                        <li class="nav-item">
+                        {{-- <li class="nav-item">
                             <a class="nav-link" href="{{ url('packages'); }}">SEO Packages</a>
-                        </li>
+                        </li> --}}
                         <!-- <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Packages
@@ -70,9 +70,9 @@
                 <li>
                     <a class="nav-link" href="{{ url('portfolio'); }}">Portfolio</a>
                 </li>
-                <li>
+                {{-- <li>
                     <a class="nav-link" href="{{ url('packages'); }}">SEO Packages</a>
-                </li>
+                </li> --}}
                 <!-- <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Packages
