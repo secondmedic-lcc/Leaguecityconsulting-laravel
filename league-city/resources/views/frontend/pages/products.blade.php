3d7e8d125c257445687455930762e4d97e40cc06
rjshah1902 <rjshah1902@gmail.com>
2024-04-09 15:45:35 +0530

    done

--- league-city/resources/views/frontend/pages/products.blade.php
+++ league-city/resources/views/frontend/pages/products.blade.php
@@ -10,36 +10,39 @@
     </div>
 </section>
 
-@foreach($product as $p) @php $url = url('products')."/".$p->url_slug; @endphp
-    <section class="products-page section-padding">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-5 left-side">
-                    <img src="{{ asset($p['product_image']); }}" alt="{{ $p['name']; }}">
-                </div>
-                <div class="col-lg-7 offset-lg-1 col-md-7 right-side">
-                    <h2 class="section-heading with-p">{{ $p['name']; }}</h2>
-                    <p class="heading-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam omnis itaque repudiandae aut libero tempora fuga labore sapiente error dignissimos! Voluptate consequuntur possimus dicta suscipit tempore ad voluptatum. Magnam, magni.</p>
-                    <a href="{{ $url; }}" class="btn web-btn">Explore More</a>
+@foreach($product as $key => $p) @php $url = url('products')."/".$p->url_slug; @endphp
+    @if($key % 2 == 0)
+        <section class="products-page section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-5 left-side">
+                        <img src="{{ asset($p['product_image']); }}" alt="{{ $p['name']; }}">
+                    </div>
+                    <div class="col-lg-7 offset-lg-1 col-md-7 right-side">
+                        <h2 class="section-heading with-p">{{ $p['name']; }}</h2>
+                        <p class="heading-info">{{ $p['description']; }}</p>
+                        <a href="{{ $url; }}" class="btn web-btn">Explore More</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    @else
+        <section class="products-page section-padding bg-dark">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7 col-md-7 right-side">
+                        <h2 class="section-heading with-p">{{ $p['name']; }}</h2>
+                        <p class="heading-info">{{ $p['description']; }}</p>
+                        <a href="{{ $url; }}" class="btn web-btn">Explore More</a>
+                    </div>
+                    <div class="col-lg-4 offset-lg-1 col-md-5 left-side">
+                        <img src="{{ asset($p['product_image']); }}" alt="{{ $p['name']; }}" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    @endif
 @endforeach
 
-<section class="products-page section-padding bg-dark">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-7 col-md-7 right-side">
-                <h2 class="section-heading with-p">7'O Clock Attendance App</h2>
-                <p class="heading-info">{{ $p['description']; }}</p>
-                <a href="#" class="btn web-btn">Explore More</a>
-            </div>
-            <div class="col-lg-4 offset-lg-1 col-md-5 left-side">
-                <img src="{{ asset('includes-frontend'); }}/images/attendanceapp.webp" alt="">
-            </div>
-        </div>
-    </div>
-</section>
 
 @include('frontend.pages.query-form')
