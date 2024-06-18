7097e82011cb47ef8b1e1e8cdb51b58387775269
nitish-secondmedic <nitish.shewani@secondmedic.com>
2024-06-18 13:45:54 +0530

    Leaguecity - campaign page - add SEO code provided by rohit

--- league-city/resources/views/frontend/pages/saas-campaign-1.blade.php
+++ league-city/resources/views/frontend/pages/saas-campaign-1.blade.php
@@ -4,6 +4,20 @@
     }
 </style>
 
+
+<script type="text/javascript">
+    (function(c, l, a, r, i, t, y) {
+        c[a] = c[a] || function() {
+            (c[a].q = c[a].q || []).push(arguments)
+        };
+        t = l.createElement(r);
+        t.async = 1;
+        t.src = "https://www.clarity.ms/tag/" + i;
+        y = l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "mtq6n04y2g");
+</script>
+
 <section class="campaign-banner">
     <img src="{{ asset('includes-frontend') }}/images/campaign/saas-campaign-banner-img.webp" alt="img" class="banner-img">
     <div class="container">
